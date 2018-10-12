8b5a1378a2a003c12b8d0eb4bf0b582958c70f8a
Wejknis <weja077@gmail.com>
2018-10-12 14:23:09 +0200

    krok 6

--- char2.c
+++ char2.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 int main () {
@@ -6,11 +5,14 @@ int main () {
     int wartosc_calkowita;
     double wartosc_wymierna;
     char ciag_znakow[256];
+    char opcja = 'x';
+    char *napis = "Opis programu. ";
 
     printf("podaj znak: ");
     scanf("%c", &znak);
     printf("podaj ciąg znakowy: ");
     scanf("%s", ciag_znakow);
+// scanf("%s", &ciag_znakow[0]); //alternatywnie
     printf("podaj liczbę całkowitą: ");
     scanf("%i", &wartosc_calkowita);
     printf("podaj liczbę wymierną: ");
@@ -18,13 +20,14 @@ int main () {
 
     printf("\n");
     printf("znak = %c,\nciąg znakowy = %s,\n"
-           "liczba całkowita = %i,\nliczba wymierna = %lf",
+           "liczba całkowita = %i,\nliczba wymierna = %lf\n",
            znak,
            ciag_znakow,
            wartosc_calkowita,
            wartosc_wymierna
+
     );
+printf("Wybrano opcję %c : %31s", opcja, napis);
 
     return 0;
-
 }
